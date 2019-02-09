8af7c9edbf15b8030bcdb3345fe57a0beca7ed30
Nathan McKinley <nmckinley@google.com>
2019-02-08 17:07:06 -0800

    Specify which things to copy over, sufficient to get a working local version of the provider.

--- third_party/terraform/utils/utils.go
+++ third_party/terraform/utils/utils.go
@@ -18,7 +18,9 @@ import (
 
 type TerraformResourceData interface {
 	HasChange(string) bool
+	GetOkExists(string) (interface{}, bool)
 	GetOk(string) (interface{}, bool)
+    Get(string) (interface{})
 	Set(string, interface{}) error
 	SetId(string)
 	Id() string
